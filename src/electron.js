43259322df9a3d43ce0eed04ec371233443ca9b0
Doughtnerd <doughtnerd@gmail.com>
2020-01-12 10:46:31 -0700

    Auto updates

--- src/electron.js
+++ src/electron.js
@@ -1,7 +1,18 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const {app, BrowserWindow, Menu, protocol, ipcMain} = require('electron');
 const path = require("path");
+const log = require('electron-log');
+const {autoUpdater} = require("electron-updater");
 
 let mainWindow;
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
+
+function sendStatusToWindow(text) {
+  log.info(text);
+  mainWindow.webContents.send('message', text);
+}
 
 function createWindow() {
   const mode = process.env.NODE_ENV;
@@ -35,6 +46,30 @@ function createWindow() {
     mainWindow = null;
   });
 }
+
+
+//Auto Updates
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
