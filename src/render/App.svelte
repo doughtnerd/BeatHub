721d51758d4c548de2e089b73e541fdd743fb9f2
Chris Carlson <doughtnerd@gmail.com>
2021-12-29 12:00:08 -0600

    Added spa router, made some text correction

--- src/render/App.svelte
+++ src/render/App.svelte
@@ -103,7 +103,6 @@
   .app-container {
     height: 100vh;
     width: 100vw;
-
     display: flex;
     flex-direction: column;
   }
@@ -118,7 +117,6 @@
 
   .main {
     overflow-y: scroll;
-
     flex-grow: 2;
   }
 
