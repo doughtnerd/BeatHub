5eeb3a70f2bb25c7e65676e2e20048cdfc5efcc5
Chris Carlson <doughtnerd@gmail.com>
2022-01-17 13:24:33 -0800

    Bugfix/db wipe on update (#41)
    * Working on fix for bug where updating the app wipes the db
    
    * possible fix up

--- src/main/db/knexfile.ts
+++ src/main/db/knexfile.ts
@@ -1,9 +1,17 @@
 import path from "path";
+import {app} from 'electron';
+
+const userDataPath = app.getPath('userData');
+console.log(userDataPath)
+
+const sqliteDBPath = process.env.NODE_ENV === 'development' ? 
+	path.join(__dirname, 'beathubdb.sqlite') : 
+	path.join(userDataPath, 'beathubdb.sqlite');
 
 const dbConnectionConfig = {
 	client: "sqlite",
 	connection: {
-		filename: "./beathubdb.sqlite"
+		filename: sqliteDBPath
 	},
 	migrations: {
 		extension: "js",
